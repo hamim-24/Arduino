bd6fba713646886a94a9d65b8ddd9e18f7c4af03
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-06-20 00:31:22 +0600

    add file

--- liquidCrystal_I2C/liquidCrystal_I2C.ino
+++ liquidCrystal_I2C/liquidCrystal_I2C.ino
@@ -1,20 +1,12 @@
-#include <Wire.h>// it defines the pin of led
-//download LiquidCrystal I2C by Frank de Brabander
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-// --LiquidCrystal_I2C address 0x27, 16 columns, 2 rows--
-//creating object of LiquidCrystal_I2C
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-//SCL in the A5
-//SDA in the A4
-//VCC must be 5v
-//GND in GND
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
-  lcd.init();             // Initialize LCD
-  lcd.backlight();        // Turn on backlightz
-  //lcd.noBacklight();        // Turn off backlightz
-  lcd.setCursor(0, 0);    // First line(x,y)
-  lcd.setCursor(0, 1);    // Second line(x,y)
+  lcd.init();             
+  lcd.backlight();        
+  lcd.setCursor(0, 0);    
+  lcd.setCursor(0, 1);   
   byte heart[8] = {
   0b00000,
   0b01010,
@@ -29,12 +21,9 @@ void setup() {
 lcd.createChar(0, heart);
 lcd.setCursor(0, 0);
 lcd.write(byte(0)); 
-lcd.print(" Hello"); // Show custom char
-  //lcd.clear();  //clear led
-  // lcd.home(); // Moves the cursor to top-left (0,0), like a reset.
+lcd.print(" Hello");
 }
 
 void loop() {
-  // You can animate or update here later
 }
 
