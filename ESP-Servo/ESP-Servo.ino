201e56f7f3a3e38ff5e288de06d4e46f0da6b0d9
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-06-19 21:53:02 +0600

    add file

--- ESP-Servo/ESP-Servo.ino
+++ ESP-Servo/ESP-Servo.ino
@@ -11,12 +11,12 @@ void loop() {
   // Sweep from 0 to 180
   for (int angle = 0; angle <= 180; angle += 10) {
     myServo.write(angle);
-    delay(500);
+    delay(50);
   }
 
   // Sweep back from 180 to 0
   for (int angle = 180; angle >= 0; angle -= 10) {
     myServo.write(angle);
-    delay(500);
+    delay(50);
   }
 }
