14204790ba620bb3287ac9d05aec8630e32831f4
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-06-25 16:29:11 +0600

    new file

--- o/o.ino
+++ o/o.ino
@@ -1,10 +1,51 @@
-HardwareSerial camSerial(2); // Use UART2 (GPIO16 = RX2, GPIO17 = TX2)
+#include <WiFi.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+const char* ssid = "Harrenhal";
+const char* password = "flat_6b@";
+
+WiFiServer server(80);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
+  Wire.begin(21, 22);
+  lcd.init();
+  lcd.backlight();
+
   Serial.begin(115200);
-  camSerial.begin(115200, SERIAL_8N1, 16, 17); // RX, TX
-  Serial.println("Sending command to CAM...");
-  camSerial.println("HELLO_CAM");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(1000);
+  Serial.print("Connected to WiFi. Local host: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
 }
 
-void loop() {}
+bool flag = false;
+
+void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    if (flag == false) {
+      Serial.println("Client connected");
+      flag = true;
+    }
+    while (client.connected()) {
+      if (client.available()) {
+        String data = client.readString();
+        Serial.println(data);
+        if (data == "1") {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("go out");
+        }
+        else {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("no one in");
+        }
+      }
+    }
+  }
+}
