659a0763158169f29e05d346cc0f6438ae06a3e7
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-07-02 15:53:34 +0600

    new readme file

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -1,24 +1,21 @@
-#define TRIG_PIN 8 //set Trig 
-#define ECHO_PIN 7//set Echo
+#define TRIG_PIN 8 
+#define ECHO_PIN 7
 
 void setup() {
-  Serial.begin(9600);//set Serial monitor(9600)
+  Serial.begin(9600);
   pinMode(TRIG_PIN, OUTPUT);//trig is output
   pinMode(ECHO_PIN, INPUT);//echo is input
 }
 
 void loop() {
-  // Send trigger pulse
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-  // Read echo pulse
   long duration = pulseIn(ECHO_PIN, HIGH);
 
-  // Convert to distance (cm)
   float distance = duration * 0.034 / 2;
 
   Serial.print("Distance: ");
