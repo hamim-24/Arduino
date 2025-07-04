5da3728ffb370277cacb1acec5a69c5d2fb8c76e
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-07-05 00:19:20 +0600

    edit file

--- sketch_jun25a/sketch_jun25a.ino
+++ sketch_jun25a/sketch_jun25a.ino
@@ -1,93 +1,30 @@
-#include <WiFi.h>
-#include <WebServer.h>
-#include <DHT.h>
 
-const char* ssid = "ESP32-DHT";
-const char* password = "12345678"; // At least 8 chars
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-WebServer server(80);
-DHT dht(26, DHT11);
+LiquidCrystal_I2C lcd(0x27,16,2);
+int pin = 2;
 
-int pin = 4;
-
-void handleRoot() {
-  char msg[1500];
-
-  snprintf(msg, 1500,
-           "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='4'/>\
-    <meta name='viewport' content='width=device-width, initial-scale=1'>\
-    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css'>\
-    <title>ESP32 DHT Server</title>\
-    <style>\
-    html { font-family: Arial; text-align: center;}\
-    h2 { font-size: 3.0rem; }\
-    p { font-size: 3.0rem; }\
-    .units { font-size: 1.2rem; }\
-    .dht-labels{ font-size: 1.5rem; padding-bottom: 15px;}\
-    </style>\
-  </head>\
-  <body>\
-      <h2>ESP32 DHT Server (Offline)</h2>\
-      <p>\
-        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
-        <span class='dht-labels'>Temperature</span>\
-        <span>%.2f</span>\
-        <sup class='units'>&deg;C</sup>\
-      </p>\
-      <p>\
-        <i class='fas fa-tint' style='color:#00add6;'></i>\
-        <span class='dht-labels'>Humidity</span>\
-        <span>%.2f</span>\
-        <sup class='units'>&percnt;</sup>\
-      </p>\
-  </body>\
-</html>",
-           readDHTTemperature(), readDHTHumidity()
-          );
-  server.send(200, "text/html", msg);
-}
-
-void setup() {
+void setup()
+{
+  Wire.begin(21, 22);
+  pinMode(pin, HIGH);
+  lcd.init();
   Serial.begin(115200);
-  dht.begin();
-  pinMode(pin, OUTPUT);
-
-  // Start in Access Point (AP) mode
-  WiFi.softAP(ssid, password);
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-
-  server.on("/", handleRoot);
-  server.begin();
-  Serial.println("HTTP server started");
+  lcd.backlight();
 }
 
-void loop() {
-  server.handleClient();
-  delay(100);
-}
-
-float readDHTTemperature() {
-  float t = dht.readTemperature();
-  if (isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return -1;
+void loop()
+{
+  digitalWrite(pin, LOW);
+  if (Serial.available()) 
+  {
+    String msg = Serial.readStringUntil('\n');
+    lcd.clear();
+    msg.trim();
+    lcd.setCursor(0,0);
+    lcd.print(msg);
   } else {
-    //Serial.println(t);
-    return t;
-  }
-}
-
-float readDHTHumidity() {
-  float h = dht.readHumidity();
-  if (isnan(h)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return -1;
-  } else {
-    //Serial.println(h);
-    return h;
+    digitalWrite(pin, HIGH);
   }
 }
