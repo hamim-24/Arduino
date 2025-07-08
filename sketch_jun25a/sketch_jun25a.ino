a0868bf7856d07b05beeeefce8462aa770999d37
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-07-08 20:24:18 +0600

    new file

--- sketch_jun25a/sketch_jun25a.ino
+++ sketch_jun25a/sketch_jun25a.ino
@@ -1,30 +1,12 @@
+#include "esp_system.h"
 
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27,16,2);
-int pin = 2;
-
-void setup()
-{
-  Wire.begin(21, 22);
-  pinMode(pin, HIGH);
-  lcd.init();
+void setup() {
   Serial.begin(115200);
-  lcd.backlight();
-}
-
-void loop()
-{
-  digitalWrite(pin, LOW);
-  if (Serial.available()) 
-  {
-    String msg = Serial.readStringUntil('\n');
-    lcd.clear();
-    msg.trim();
-    lcd.setCursor(0,0);
-    lcd.print(msg);
+  if(psramFound()){
+    Serial.println("✅ PSRAM is working!");
   } else {
-    digitalWrite(pin, HIGH);
+    Serial.println("❌ PSRAM NOT detected!");
   }
 }
+
+void loop() {}
