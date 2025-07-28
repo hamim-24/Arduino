39a3a20e5e3a14b56aa7e591d223a5b2aca69351
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-07-28 11:11:20 +0600

    Temp commit before filter-branch

--- Serial-monitor-control/Serial-monitor-control.ino
+++ Serial-monitor-control/Serial-monitor-control.ino
@@ -1,22 +1,11 @@
-
-String cmd = "";
-
-void setup() {
+void setup(){
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available()) {
-    cmd = Serial.readString();
-    if (cmd == "on") {
-      digitalWrite(2, HIGH);
-      Serial.println("light on");
-    }
-    else if (cmd == "off") {
-      digitalWrite(2, LOW); 
-      Serial.println("light off");
-    }
-  }
+void loop(){
+  int air = analogRead(34);
 
+  Serial.print("air Quality : ");
+  Serial.println(air);
+  delay(1000);
 }
